fc7b134e2860ced02e98f57a11fa9f7ee6aef63e
David McConnell <dvmcconnell@gmail.com>
2022-04-13 18:09:47 -0600

    WIP script with completed functionality, to be tested

---
+++ team_approval/README.md
@@ -0,0 +1,24 @@
+## Team Approval Docker Image
+
+**Version 1**
+
+This docker image will take as input a repository, pull request ID, and list of team names and determine if the PR has approvals from at least 1 member of each team. This docker image is intended to be run in the context of a github action to gate pull requests behind appropriate reviews across different teams in Digital Science.
+
+### Building the image
+
+```
+docker build -f team_approval/Dockerfile -t team_approval:v<version>
+```
+
+### Running the image
+
+```
+docker run team_approval:<version> -r <repo> -p <pull_request_id> -t <team_1>,<team_2>,...
+```
+
+If the appropriate teams have approved the pull request, the process will exit with a code of 0. If not, the image will exit with a code of 1.
+
+### Updating the image
+
+#. Increment the version listed at the top of this README
+#. Change the version specified in the github action under `.github/workflows`
---
+++ team_approval/get_team_approval_status.py
@@ -0,0 +1,118 @@
+"""Determine if a PR has approvals from specific teams"""
+
+import argparse
+from collections import defaultdict
+from typing import List
+import sys
+
+import github
+
+GENAPSYS_GITHUB = "GenapsysInc"
+
+APPROVED = "APPROVED"
+CHANGES_REQUESTED = "CHANGES_REQUESTED"
+
+
+class ConfigurationError(Exception):
+    """Should be raised when input arguments are found to be incorrect/invalid"""
+
+
+def team_member_has_approved_pr(team: github.Team.Team, pull: github.PullRequest.PullRequest) -> bool:
+    """Determine if a member of the given team as approved the given pull request
+
+    :param team: The Team in question
+    :param pull: The PullRequest in question
+    :return: True if a member of the given team has approved the PR, False if not
+    """
+    team_members = team.get_members()
+
+    # Get list of reviews sorted by user
+    reviews_by_user = defaultdict(list)
+
+    # Retrieved in chronological order from GitHub
+    for review in pull.get_reviews():
+        # Ignore any reviews by users not in the relevant team
+        if review.user in team_members:
+            reviews_by_user[review.user.id].append(review)
+
+    # Go over each users review in reverse chronilogical order, ignoring plain comments and handling
+    # approvals and change requests accordingly
+    for reviews in reviews_by_user.values():
+        for review in reversed(reviews):
+            # At least one member of the team's last PR review of note was an approval
+            if review.state == APPROVED:
+                print(f'Team "{team.name}" approval condition was met')
+                return True
+
+            # This member's last review of note was a change request so stop looking
+            if review.state == CHANGES_REQUESTED:
+                break
+
+    print(f'Team "{team.name}" approval condition was NOT met')
+    return False
+
+
+def pr_has_appropriate_reviews(secret: str, repo: str, pr_num: int, team_names: List[str]) -> bool:
+    """Given a repository, PR number, and list of teams, determine if the given PR has at least one
+    approval from each of the listed teams
+
+    :param secret: Github secret/PAT for authentication
+    :param repo: The GitHub repo to inspect
+    :param pr_num: The Pull Request number in the given repo
+    :param team_names: The list of teams that are required to be approvers
+    :return: True if the PR has appropriate reviews, False if not
+    """
+    # Ignore caps for downstream lookup
+    team_names = [team_name.lower() for team_name in team_names]
+
+    client = github.Github(secret)
+
+    try:
+        org = client.get_organization(GENAPSYS_GITHUB)
+    except github.GithubException as failed_org_query:
+        raise ConfigurationError("Could not authenticate with given secret") from failed_org_query
+
+    teams = [team for team in org.get_teams() if team.name.lower() in team_names]
+    print([team.name for team in org.get_teams()])
+
+    missing_teams = set(team_names) - {team.name.lower() for team in teams}
+
+    if missing_teams:
+        raise ConfigurationError(f"Could not find these teams: {', '.join(missing_teams)}")
+
+    try:
+        pull = client.get_repo(repo).get_pull(pr_num)
+    except github.GithubException as failed_pull_query:
+        raise ConfigurationError(f"Could not find PR with number {pr_num} in repo {repo}") from failed_pull_query
+
+    all_teams_approved = True
+
+    # Do this instead of an all so that each team's result is processed and output
+    for team in teams:
+        all_teams_approved &= team_member_has_approved_pr(team, pull)
+
+    return all_teams_approved
+
+
+def parse_args():
+    """Parse commandline args"""
+    parser = argparse.ArgumentParser(description="Determine if relevant teams have approved a PR")
+
+    parser.add_argument("-s", "--secret", required=True, help="GitHub token for authentication")
+
+    parser.add_argument("-r", "--repo", required=True, help="The repository to inspect")
+
+    parser.add_argument("-p", "--pull-request", type=int, required=True, help="The PR to inspect")
+
+    parser.add_argument("-t", "--teams", required=True, nargs="+", help="Teams required for approval")
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    opts = parse_args()
+
+    if pr_has_appropriate_reviews(opts.secret, opts.repo, opts.pull_request, opts.teams):
+        sys.exit(0)
+
+    sys.exit(1)
