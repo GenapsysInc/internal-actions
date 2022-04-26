5ea0e97cc9f385893c1dd9c9bf11d6fa811d8f8f
David McConnell <dvmcconnell@gmail.com>
2022-04-26 10:00:15 -0600

    move test up a dir

---
+++ action_utils/tests/utils/gitpython_utils.py
@@ -0,0 +1,5 @@
+"""Utilities for testing GitPython usage, mocking of a lot of GitPyhon classes"""
+
+__author__ = "David McConnell"
+__credits__ = ["David McConnell"]
+__maintainer__ = "David McConnell"
