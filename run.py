053621f377b3dd52d8cc56c7d7db811068a1d0eb
David McConnell <dvmcconnell@gmail.com>
2022-11-07 11:29:49 -0700

    update tools/utils

--- run.py
+++
@@ -1,9 +0,0 @@
-from action_utils.check_version import temp_name
-import github
-import os
-
-secret = os.environ["GITHUB_SECRET"]
-
-client = github.Github(secret)
-
-temp_name(client, "GenapsysInc", "internal-actions")
