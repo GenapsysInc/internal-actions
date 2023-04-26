e99c6ae677c4e9acb0d81ca017fcebdf3a66db9f
dvmcconnell <dvmcconnell@gmail.com>
2023-04-26 11:10:41 -0600

    copy

--- action_utils/patch_pluggy_manager.py
+++ action_utils/patch_pluggy_manager.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-import shutil.copy
+from shutil import copy
 
 import pluggy._manager
 manager_path = Path(pluggy._manager.__file__)
 
-shutil.copy(Path(__file__).parent / "patch/_manager.py", manager_path)
+copy(Path(__file__).parent / "patch/_manager.py", manager_path)
