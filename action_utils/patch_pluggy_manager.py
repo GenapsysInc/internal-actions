55b88e4a47b66f654e9c0d0ae8951d2f322035d8
dvmcconnell <dvmcconnell@gmail.com>
2023-04-26 12:24:43 -0600

    unpatch

--- action_utils/patch_pluggy_manager.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-from shutil import copy
-
-import pluggy._manager
-manager_path = Path(pluggy._manager.__file__)
-
-copy(Path(__file__).parent / "patch/_manager.py", manager_path)
